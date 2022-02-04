c9c61adaa499d5155333008ec52d0f22fbc33cff
Vanessa Fadase <89610594+Ms-CodingPanda@users.noreply.github.com>
2022-02-03 19:35:31 -0700

    Corrected latitude and longitude code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,8 +24,8 @@ export default function WeatherForecast(props) {
     );
   } else {
     let apiKey = "ada1f79db1942604234797e2dd089b48";
-    let longitude = props.coord.lon;
-    let latitude = props.coord.lat;
+    let longitude = props.coords.lon;
+    let latitude = props.coords.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
     axios.get(apiUrl).then(handleResponse);
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <WeatherIcon code={props.data.icon} size={52} />
+            <WeatherIcon code={props.data.icon} size={65} />
             <WeatherTemperature fahrenheit={props.data.temperature} />
           </div>
         </div>
